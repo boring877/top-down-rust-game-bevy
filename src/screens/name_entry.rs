64ef29d8038279ca3468affeba8f45a3bcaeb6b5
boring877 <elfhuyace@gmail.com>
2026-02-13 10:49:57 +0000

    added main entry

--- src/screens/name_entry.rs
+++ src/screens/name_entry.rs
@@ -1,25 +1,118 @@
-//This is name entry for the game Player vs Boss
-
-use bevy::prelude::*;
 use crate::game_state::GameState;
+use bevy::input::keyboard::{Key, KeyboardInput};
+use bevy::prelude::*;
 
+#[derive(Resource)]
+struct PlayerName(String);
+
+#[derive(Component)]
+struct NameText;
+
+#[derive(Component)]
+struct NameEntryUI;
 
 pub fn name_entry_plugin(app: &mut App) {
-    app
-        
-        //Run once on entry !!!
+    app.insert_resource(PlayerName(String::new()))
         .add_systems(OnEnter(GameState::NameEntry), setup_name_entry)
-
-        //Run every frame while in this state !!!
-        .add_systems(Update, handle_name_input.run_if(in_state(GameState::NameEntry)));
-
+        .add_systems(
+            Update,
+            (handle_keyboard_input, handle_start_button).run_if(in_state(GameState::NameEntry)),
+        )
+        .add_systems(OnExit(GameState::NameEntry), cleanup_name_entry);
 }
-
 
 fn setup_name_entry(mut commands: Commands) {
-    
+    commands.spawn(Camera2d);
+
+    commands.spawn((
+        NameEntryUI,
+        Node {
+            width: percent(100),
+            height: percent(100),
+            align_items: AlignItems::Center,
+            justify_content: JustifyContent::Center,
+            flex_direction: FlexDirection::Column,
+            row_gap: px(20),
+            ..default()
+        },
+        children![
+            (
+                Text::new("Enter your name:"),
+                TextFont {
+                    font_size: 40.0,
+                    ..default()
+                },
+                TextColor(Color::WHITE),
+            ),
+            (
+                Text::new(""),
+                TextFont {
+                    font_size: 40.0,
+                    ..default()
+                },
+                TextColor(Color::WHITE),
+                NameText,
+            ),
+            (
+                Button,
+                Node {
+                    width: px(200),
+                    height: px(60),
+                    justify_content: JustifyContent::Center,
+                    align_items: AlignItems::Center,
+                    ..default()
+                },
+                BackgroundColor(Color::srgb(0.2, 0.2, 0.6)),
+                children![
+                    Text::new("Start"),
+                    TextFont {
+                        font_size: 40.0,
+                        ..default()
+                    },
+                    TextColor(Color::WHITE),
+                ]
+            )
+        ],
+    ));
 }
 
-fn handle_name_input(mut commands: Commands) {
-    
+fn cleanup_name_entry(
+    mut commands: Commands,
+    query: Query<Entity, With<NameEntryUI>>,
+    cameras: Query<Entity, With<Camera2d>>,
+) {
+    for entity in query.iter() {
+        commands.entity(entity).despawn();
+    }
+    for cam in cameras.iter() {
+        commands.entity(cam).despawn();
+    }
+}
+
+fn handle_keyboard_input(
+    mut events: MessageReader<KeyboardInput>,
+    mut player_name: ResMut<PlayerName>,
+    mut name_text: Single<&mut Text, With<NameText>>,
+) {
+    for event in events.read() {
+        if event.state.is_pressed() {
+            if event.logical_key == Key::Backspace {
+                player_name.0.pop();
+            } else if let Some(text) = event.text.as_ref() {
+                player_name.0.push_str(&text);
+            }
+            **name_text = player_name.0.clone().into();
+        }
+    }
+}
+
+fn handle_start_button(
+    mut interaction_query: Query<&Interaction, (Changed<Interaction>, With<Button>)>,
+    mut next_state: ResMut<NextState<GameState>>,
+) {
+    for interaction in interaction_query.iter_mut() {
+        if *interaction == Interaction::Pressed {
+            next_state.set(GameState::Game);
+        }
+    }
 }
--- src/screens/splash.rs
+++ src/screens/splash.rs
@@ -1,58 +1,63 @@
 use bevy::prelude::*;
 use crate::game_state::GameState;
 
-
 #[derive(Resource, Deref, DerefMut)]
 struct SplashTimer(Timer);
 
+#[derive(Component)]
+struct SplashUI;
 
 pub fn splash_plugin(app: &mut App) {
     app
         .add_systems(OnEnter(GameState::Splash), splash_setup)
-        .add_systems(Update, countdown.run_if(in_state(GameState::Splash)));
+        .add_systems(Update, countdown.run_if(in_state(GameState::Splash)))
+        .add_systems(OnExit(GameState::Splash), cleanup_splash);
 }
 
 fn splash_setup(mut commands: Commands) {
-    //spawn the camera !
-
     commands.spawn(Camera2d);
 
-    // spawn UI with title
-
     commands.spawn((
-
-        //node for the UI
+        SplashUI,
         Node {
-            width: percent (100),
-            height: percent (100),
+            width: percent(100),
+            height: percent(100),
             align_items: AlignItems::Center,
             justify_content: JustifyContent::Center,
             ..default()
         },
-        children! [(
+        children![(
             Text::new("Player VS Boss"),
             TextFont {
                 font_size: 80.0,
                 ..default()
             },
             TextColor(Color::WHITE),
-            
-        )
-            
-        ]
-
+        )],
     ));
 
-    // Timer for 2 seconds
-    commands.insert_resource(SplashTimer(Timer::from_seconds(2.0, TimerMode::Once)))
-} 
+    commands.insert_resource(SplashTimer(Timer::from_seconds(2.0, TimerMode::Once)));
+}
 
 fn countdown(
-    time:Res<Time>, // time global shard resource !!
-    mut timer:ResMut<SplashTimer>, // timer write resource !!
-    mut next_state: ResMut<NextState<GameState>>, //change game state
+    time: Res<Time>,
+    mut timer: ResMut<SplashTimer>,
+    mut next_state: ResMut<NextState<GameState>>,
 ) {
     if timer.tick(time.delta()).just_finished() {
         next_state.set(GameState::NameEntry);
+    }
+}
+
+fn cleanup_splash(
+    mut commands: Commands,
+    query: Query<Entity, With<SplashUI>>,
+    cameras: Query<Entity, With<Camera2d>>,
+) {
+    for entity in query.iter() {
+        commands.entity(entity).despawn();
+    }
+    for cam in cameras.iter() {
+        commands.entity(cam).despawn();
     }
 }
